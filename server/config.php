527588b95ce3d2cdd3b3dbae8ef8f02f8f8472b9
Hans Sean Nathanael <hans.sean22@gmail.com>
2022-12-20 18:24:45 +0700

    Hapus code tidak terpakai

--- server/config.php
+++ server/config.php
@@ -9,7 +9,4 @@
     if(!$connection) {
         die("Failed to connect to database: " . mysqli_connect_error());
     }
-
-    $izin_admin = "-ce-iAH70YcLc6Yk";
-    $izin_anggota = "TDC65o5GHlHWOdW4";
 ?>
